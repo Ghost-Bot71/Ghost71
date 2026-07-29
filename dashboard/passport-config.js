7adc4e861c0995fe31665b0b0243e976af50a727
negative xalman <onlygoatbot.nx@gmail.com>
2026-07-29 20:55:23 +0600

    Intial commit

--- dashboard/passport-config.js
+++ dashboard/passport-config.js
@@ -1,34 +1,47 @@
 const localStrategy = require('passport-local').Strategy;
 
-module.exports = function (Passport, db, bcrypt) {
-	Passport.serializeUser((user, done) => {
-		done(null, user.email);
-	});
+module.exports = function (Passport, db, bcrypt, sessionStore) {
+Passport.serializeUser((user, done) => {
+done(null, user.email);
+});
 
-	Passport.deserializeUser(async (email, done) => {
-		const user = await db.get(email);
-		done(null, user);
-	});
+Passport.deserializeUser(async (email, done) => {  
+    try {  
+        const user = await db.get(email);  
+        done(null, user);  
+    } catch (err) {  
+        done(err, null);  
+    }  
+});  
 
-	Passport.use(new localStrategy({
-		usernameField: "username",
-		passwordField: "password",
-		passReqToCallback: true
-	}, async function (req, email, password, done) {
-		const user = await db.get(email);
-		if (!user)
-			return done(null, false, { message: "Email không tồn tại" });
+Passport.use(new localStrategy({  
+    usernameField: "email",         
+    passwordField: "password",  
+    passReqToCallback: true  
+}, async function (req, email, password, done) {  
+    try {  
+        const user = await db.get(email);  
+        if (!user)  
+            return done(null, false, { message: "Email không tồn tại" });  
 
-		const isMatch = await bcrypt.compare(password, user.password);
-		if (!isMatch)
-			return done(null, false, { message: "Địa chỉ email hoặc mật khẩu không đúng" });
+        const isMatch = await bcrypt.compare(password, user.password);  
+        if (!isMatch)  
+            return done(null, false, { message: "Địa chỉ email hoặc mật khẩu không đúng" });  
 
-		const remeber = req.body.remeber;
-		if (remeber)
-			req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000; // 30 days
-		else
-			req.session.cookie.expires = false;
+        const remember = req.body.remember || req.body.remember_me;  
+          
+        if (remember) {  
+            req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000;  
+            req.session.cookie.expires = new Date(Date.now() + (30 * 24 * 60 * 60 * 1000));  
+        } else {  
+            req.session.cookie.maxAge = null;  
+            req.session.cookie.expires = null;  
+        }  
 
-		return done(null, user);
-	}));
+        return done(null, user);  
+    } catch (err) {  
+        return done(err);  
+    }  
+}));
+
 };
