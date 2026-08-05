b14e9df5c6e486299a4e320c2172dec765297038
negative xalman <onlygoatbot.nx@gmail.com>
2026-08-05 22:48:29 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -1,12 +1,12 @@
 module.exports = {
   config: {
     name: "fork",
-    version: "2.8",
+    version: "3.0",
     author: "xalman",
     countDown: 5,
     role: 0,
-    shortDescription: "Show  repository info",
-    category: "utils",
+    shortDescription: "Show repository info",
+    category: "utility",
     guide: {
       en: "{p}fork"
     }
@@ -14,18 +14,23 @@ module.exports = {
 
   langs: {
     en: {
-      current: "╭━━━〔 𝐆𝐎𝐀𝐓-𝐁𝐎𝐓-𝐕𝟐 〕━━━⦿\n┃\n┃ 👑 𝗢𝘄𝗻𝗲𝗿: 𝖷𝖺𝗅𝗆𝖺𝗇 𝖧𝗈𝗌𝗌𝖺𝗂𝗇\n┃ 🔗 𝗥𝗲𝗽𝗼: %1\n┃ 💎 𝗦𝘁𝗮𝘁𝘂𝘀: always updating\n┃\n┃ ❝ 𝖳𝗁𝖾 𝖦𝗋𝖾𝖺𝗍𝖾𝗌𝗍 𝖮𝖿 𝖠𝗅𝗅 𝖳𝗂𝗆𝖾 𝖠𝗎𝗍𝗈𝗆𝖺𝗍𝗂𝗈𝗇 ❞\n┃\n╰━━━━━━〔 📥 〕━━━━━━⦿"
+      current: `📌 𝐍𝐗-𝐆𝐎𝐀𝐓-𝐁𝐎𝐓-𝐕𝟑
+━━━━━━━━━━━━━━━━━━━━━━━━
+👑 Repo Owner : 𝖷𝖺𝗅𝗆𝖺𝗇 𝖧𝗈𝗌𝗌𝖺𝗂𝗇
+🔗 Repo       : %1
+💎 Status     : always updating
+━━━━━━━━━━━━━━━━━━━━━━━━`
     }
   },
 
   onStart: async function ({ message, getLang }) {
-    const link = "https://github.com/goatbotnx/GOAT-BOT-V2";
+    const link = "https://github.com/goatbotnx/NX-GOAT-BOT-V3";
     return message.reply(getLang("current", link));
   },
 
   onChat: async function ({ message, getLang, event }) {
     if (event.body && event.body.toLowerCase() === "fork") {
-      const link = "https://github.com/goatbotnx/GOAT-BOT-V2";
+      const link = "https://github.com/goatbotnx/NX-GOAT-BOT-V3";
       return message.reply(getLang("current", link));
     }
   }
